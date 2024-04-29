fcad675502f055cb7f27e07acd5b055931818d95
Matthew Fritsch <matthewmfritsch@gmail.com>
2024-04-28 23:00:56 -0700

    drastically simplified main file

--- main.go
+++ main.go
@@ -1,30 +1,13 @@
 package main
 
 import (
-    "fantasy-valorant/routes"
     "github.com/gin-gonic/gin"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "gorm.io/gorm/logger"
-    "os"
-    "strings"
+    "fantasy-valorant/routes"
 )
 
-func InitDB() *gorm.DB {
-    bdsn, _ := os.ReadFile(".env")
-    dsn := strings.Trim(string(bdsn), "\n")
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-        Logger: logger.Default.LogMode(logger.Info),
-    })
-    if err != nil {
-        return nil
-    }
-    return db
-}
-
 func main() {
-    db := InitDB()
+    gin.SetMode(gin.DebugMode)
     router := gin.Default()
-    routes.SetupRouter(router, db)
+    routes.SetupRouter(router)
     router.Run(":8080")
 }
