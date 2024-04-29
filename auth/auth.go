cdc850fc939e7b3acee7eea3607e97fefbc51a1a
Matthew Fritsch <matthewmfritsch@gmail.com>
2024-04-29 00:36:30 -0700

    return access token on signin

--- auth/auth.go
+++ auth/auth.go
@@ -6,6 +6,7 @@ import (
     "net/http"
     supa "github.com/nedpals/supabase-go"
     "github.com/gin-gonic/gin"
+    "strings"
 )
 
 func Register(supabase *supa.Client, c *gin.Context) {
@@ -43,16 +44,21 @@ func SignIn(supabase *supa.Client, c *gin.Context) {
     }
 
     ctx := context.Background()
-    user, err := supabase.Auth.SignIn(ctx, supa.UserCredentials{
+    session, err := supabase.Auth.SignIn(ctx, supa.UserCredentials{
         Email:    email,
         Password: password,
     })
 
     if err != nil {
         log.Printf("Error logging in user: %v\n", err)
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error logging in user"})
+        // Check if it's an authentication error
+        if strings.Contains(err.Error(), "Invalid credentials") {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+        } else {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Error logging in user"})
+        }
         return
     }
 
-    c.JSON(http.StatusOK, gin.H{"user": user})
+    c.JSON(http.StatusOK, gin.H{"accessToken": session.AccessToken})
 }
